9ce8c22aa2808e5d8cc1506b6c5e4c6318a9b446
Faiz <faizahmadarfi@gmail.com>
2021-06-26 16:39:28 +0530

    renamed

--- collage hw/multiplication.c
+++ collage hw/multiplication.c
@@ -2,7 +2,7 @@
 int main ()
 {
     int a,b,c;
-    printf("Enter therr numbers\n");
+    printf("Enter three numbers\n");
     scanf("%d%d%d",&a,&b,&c);
     printf("The multiplication is %d",a*b*c);
 }
