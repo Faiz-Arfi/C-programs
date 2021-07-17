6025fee16d84107d6757f1e41c1b11c0939f5f2f
Faiz <faizahmadarfi@gmail.com>
2021-07-17 11:34:36 +0530

    added hw

---
+++ collage hw/swaping.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b,c;
+    printf("Enter two numbers\n");
+    scanf("%d%d",&a,&b);
+    printf("a = %d  and b= %d \n",a,b);
+
+    printf("swaping the numbers\n");
+    printf("a = %d  and b= %d \n",b,a);
+}
